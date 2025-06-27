c5434596065aee9153d404e0be11dc21c37ab750
hassandogo <hassanmahamatdogo@gmail.com>
2025-06-27 11:32:08 +0100

    feat: resolution des commentaires et suppression de rendez-vous

--- src/main/java/com/clinicapp/backend/model/core/Patient.java
+++ src/main/java/com/clinicapp/backend/model/core/Patient.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
