e79924e5f9db9be6022d86017aaa6f8d17183d18
loic.k <loickenmoe1@gmail.com>
2025-09-27 10:21:53 +0100

    fix: conflicts revolve from feature/chat-implementation to develop

---
+++ src/main/java/com/clinicapp/backend/controller/SecretaryDashboardController.java
@@ -0,0 +1,13 @@
+package com.clinicapp.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.prefix}/secretary/dashboard")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('SECRETARY')")
+public class SecretaryDashboardController {
+}
